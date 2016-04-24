4e8e39a7823cfc6c9facf1b186117dd061cff71a
Erin Mahoney <emahoneyphoto@gmail.com>
2016-04-24 12:07:50 -0700

    Added logic for completed tasks on reload

--- script.js
+++ script.js
@@ -8,23 +8,35 @@ $(function() {
   
   function addDoneClickHandler(doneButton) {
     doneButton.on('click', function() {
+      var taskIndex = $(this).parent('li').attr('data-task-index');
+      var task = tasks[taskIndex];
+
       $(this).parent('li').toggleClass('done');
       if ($(this).parent('li').hasClass('done')) {
+        task.done = true;
         $('.notification').fadeIn();
       } else {
+        task.done = false;
         $('.notification').fadeOut();
       }
+      
+      localStorage.tasks = JSON.stringify(tasks);
     });
   }
   
-  function addTaskToList(val) {
-    var newListItem = $("<li class='listItem align-right'>" + val + "</li>");
+  function addTaskToList(newTask, taskIndex) {
+    var newListItem = $("<li class='listItem align-right'>" + newTask.text + "</li>");
+    newListItem.attr('data-task-index', taskIndex);
+    if (newTask.done === true) {
+      newListItem.addClass('done');
+    }
+
     var doneButton = $("<a href='javascript:void(0)' class='done-btn'><i class='fa fa-check-circle-o fa-2x' aria-hidden='true'></i></a>");
     addDoneClickHandler(doneButton);
     newListItem.append(doneButton);
     
     //check for empty inputs and spaces
-    if(val.length>0 && val.trim()) {
+    if(newTask.text.length>0 && newTask.text.trim()) {
       $('#list').append(newListItem);
       $('li:odd').addClass('left');
       $('li:even').addClass('right');
@@ -32,7 +44,7 @@ $(function() {
   }
   //iterate and add to-do value to list
   for(var i = 0; i < tasks.length; i++) {
-    addTaskToList(tasks[i]);
+    addTaskToList(tasks[i], i);
   }
   
   var addTask = function() {
@@ -40,13 +52,18 @@ $(function() {
     var val = $('#todo').val();
     
     // add the task to the array
-    tasks.push(val);
+    var newTask = {
+      text: val,
+      done: false
+    };
+    tasks.push(newTask);
     
     // save to local storage
     localStorage.tasks = JSON.stringify(tasks);
     
+    var taskIndex = tasks.length - 1;
     // append the to-do to the list
-    addTaskToList(val);
+    addTaskToList(newTask, taskIndex);
     
     // reset the input field and focus it.
     $('#todo').val("").focus();
